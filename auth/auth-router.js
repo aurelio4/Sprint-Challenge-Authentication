017713521d09a1c7b854c88666a2727a0c78588e
aurelio4 <aurelioarcabascio@gmail.com>
2020-05-22 14:13:33 -0400

    register endpoint done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,62 @@
 const router = require('express').Router();
+const Auth = require('./auth-model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const rounds = process.env.BCRYPT_ROUNDS || 8
+
+    if(!username || !password) {
+      res.status(400).json({ error: 'missing data' })
+    } else {
+      const hash = bcrypt.hashSync(password, rounds)
+      const newUser = { username, password: hash }
+      const token = createToken(newUser)
+      const addUser = await Auth.createUser(newUser)
+      res.status(201).json({ addUser, token })
+    }
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({ error: "server error, sorry" })
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body
+    if (username && password) {
+      const user = await Auth.findBy({ username })
+      console.log(user.password)
+      console.log(password)
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = createToken(user)
+        res.status(200).json({ success: 'logged in', token })
+      } else {
+        res.status(400).json({ error: "missing data" })
+      }
+    } else {
+      res.status(400).json({ error: "missing data" })
+    }
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "server error, sorry" })
+  }
 });
+
+function createToken(user) {
+  const jwtSecret = process.env.JWT_SECRET || 'jwtsecret123'
+  const payload = {
+    sub: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, jwtSecret, options)
+}
 
 module.exports = router;
